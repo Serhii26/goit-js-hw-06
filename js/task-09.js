c0965aed493cd6933d1e10721a6c94a37578e3e2
Serhii26 <25kontrakt@gmail.com>
2022-10-11 11:26:46 +0300

    commit3

--- js/task-09.js
+++ js/task-09.js
@@ -2,15 +2,12 @@ const bodyEl = document.querySelector('body');
 const spanEl = document.querySelector('.color');
 const buttonEl = document.querySelector('.change-color');
 
- 
-
 buttonEl.addEventListener('click', changeColor );
 function changeColor (){
   const change = getRandomHexColor();
   bodyEl.style.backgroundColor = change;
   spanEl.textContent = change;
 }
- 
  
  function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
